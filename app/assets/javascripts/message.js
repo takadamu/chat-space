4e2f861f9d3beeb865e4c39cd672c7de4132bc62
takadamu <0rg68v3m326626f@gmail.com>
2020-02-16 16:12:48 +0900

    chat-space  Automatic update function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,4 @@
 $(function(){
-  last_message_id = $('.main-chat__message-list__messages:last').data("message-id");
-  console.log(last_message_id);
 
     function buildHTML(message){
       if ( message.image ) {
@@ -64,5 +62,31 @@ $(function(){
       .fail(function(){
         alert('error');
       })  
-  })
+  });
+
+  var reloadMessages = function() {
+    last_message_id = $('.main-chat__message-list__messages:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = ''
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+       }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+  }
 });
